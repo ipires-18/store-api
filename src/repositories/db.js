56d59871f8be29f9573b9539ca6770210757e046
Ian Oliveira <ian.oliveira@einstein.br>
2022-05-30 22:48:37 -0300

    feat: add dotenv more gitignore

--- src/repositories/db.js
+++ src/repositories/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const pg = require("pg")
 
 async function connect(){
   if(global.connection) return global.connection.connect()
 
   const pool = new pg.Pool({
-    connectionString:"postgres://ayskkehq:utIjjeFfNzxtfcwMQ8n5RE-_nHKcMGqt@castor.db.elephantsql.com/ayskkehq"
+    connectionString:process.env.DBconnection
   })
 
   global.connection = pool;
